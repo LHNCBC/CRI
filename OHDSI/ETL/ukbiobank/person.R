c0c3ec36a88bc5bc9747a21dd23062bbe565a26f
Vojtech Huser <vojtech.huser@gmail.com>
2020-10-08 09:37:45 -0400

    Update person.R

--- OHDSI/ETL/ukbiobank/person.R
+++ OHDSI/ETL/ukbiobank/person.R
@@ -5,6 +5,8 @@ library(data.table)
 
 # -----Person table
 file<-'ukb43407.csv'
+#this file is huge, it is 36GB !!!!!
+#the fread approach works the best
 person<-fread(file, select=c('eid','31-0.0', '34-0.0', '52-0.0','40000-0.0' ))
 
 colnames(person)<-c('person_id', 'gender_source_value', 'year_of_birth', 'month_of_birth',  'death_date_time')
