a081c3070fc246d9899184e1416b21aeae58c4d1
cmayer2 <43854926+cmayer2@users.noreply.github.com>
2021-04-22 08:47:37 -0400

    Update functions.R

--- AoU/r4aou/R/functions.R
+++ AoU/r4aou/R/functions.R
@@ -65,7 +65,7 @@ aou_run <-function(sql){
   cdmDatabaseSchema=Sys.getenv('WORKSPACE_CDR')
   sql <- SqlRender::render(sql,cdmDatabaseSchema=cdmDatabaseSchema)
   sql <- SqlRender::translate(sql,targetDialect = 'bigquery')
-  sql=stringr::str_replace_all(sql,'r2019q4r3','R2019Q4R3')
+  sql=stringr::str_replace_all(sql,'r2020q4r2','R2020Q4R2')
   cat(sql)
   q <- bigrquery::bq_project_query(billing, sql)
   out<-bigrquery::bq_table_download(q)
@@ -195,19 +195,28 @@ execute_cohort <-function(cohortID){
   sql=getCohortSql(aa,baseUrl)
   
   #cohort edits
+  # Declare database schema
   sql2= gsub("@cdm_database_schema", "@cdmDatabaseSchema", sql)
+  #Remove result schema since not present
   sql2= gsub("@results_database_schema.", "", sql2)
+  #declare vocabulary schema as same as database schema
   sql2= gsub("@vocabulary_database_schema", "@cdmDatabaseSchema", sql2)
+  #Remove deletion since table does not already exist
   sql2=gsub("DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id;","",sql2)
+  #create final table with select statment
   sql2=gsub("select @target_cohort_id as cohort_definition_id, person_id, start_date, end_date", "create temp table #target_cohort_table AS (select cohort_definition_id as target_cohort_id, co.person_id as subject_id, co.start_date as cohort_start_date, co.end_date as 
 cohort_end_date ", sql2) 
+  # Add parentheses at end of table creation
   sql2=gsub("FROM #final_cohort CO", "FROM #final_cohort CO)", sql2)
+  # Remove irrelevent parts
   sql2=gsub("delete from cohort_censor_stats where cohort_definition_id = @target_cohort_id;","",sql2)
+  #generate output since no result schema exist
   sql2= paste(sql2, "
 select * from #target_cohort_table")
   sql2 <- SqlRender::render(sql2,cdmDatabaseSchema=cdmDatabaseSchema)
   sql2 <- SqlRender::translate(sql2,targetDialect = 'bigquery')
   sql2=stringr::str_replace_all(sql2,'r2020q4r2','R2020Q4R2')
+  # state temp table
   sql5= gsub("create table", "CREATE TEMP TABLE", sql2)
   sql5= gsub("CREATE TABLE", "CREATE TEMP TABLE", sql5)
   sql5=gsub("insert into @target_database_schema.@target_cohort_table ","",sql5)
